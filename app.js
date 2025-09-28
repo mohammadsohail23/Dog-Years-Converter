67062186473af1359c18526516ace61046f24b60
Mohammad Z. Sohail <159181150+mohammadsohail23@users.noreply.github.com>
2025-09-28 06:05:03 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// Creating a variable of my age and setting it to my age as a number.
+const myAge = 19;
+
+// Creating a variable that will change later on.
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+// Taking myAge variable and subtracting 2 from it. And then, setting the result equal to laterYears.
+laterYears = myAge - 2;
+
+// Multiplying the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.
+laterYears = laterYears * 4;
+
+// This line creates a variable that stores the sum of earlyYears and laterYears, which represents your age in dog years.
+let myAgeInDogYears = earlyYears + laterYears;
+
+// This stores your name in all lowercase letters.
+const myName = "ZAYEDSOHAIL".toLowerCase();
+
+// Writing a console.log statement that displays my name and age in dog years.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
